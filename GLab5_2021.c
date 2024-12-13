43b69bf92c030a94dbabceb4626e84824782d08f
Kacper Frączek <kacper@tee.pl>
2024-12-13 01:25:06 +0100

    GLab5 part 2

--- GLab5_2021.c
+++ GLab5_2021.c
@@ -42,10 +42,23 @@ void print_wishlist(const wishlist* w){
 }
 void destroy_wishlist(wishlist *w){
         if (w != NULL && w->data != NULL) {
+            return;
+        }
             free(w->data);
             w->data = NULL;
             w->length = 0;
 }
 int append_presents(wishlist *w, const char *l){
-    
+    if (l == NULL || *l == '\0') {
+        return 0; // Nothing to append
+    }
+    size_t new_length = strlen(l)+w->length;
+    char *new_data = (char*)realloc(w->data,new_length*sizeof(char));
+    if (new_data == NULL) {
+        return -1; // Memory allocation failed
+    }
+    memcpy(new_data+w->length,l,strlen(l));
+    w->data = new_data;
+    w->length = new_length;
+
 }
